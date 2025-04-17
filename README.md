bb46126d367bd5b832d93d98423e4aee36653896
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-16 21:43:18 -0400

    added hex

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 prodtest/bin
 - Copy all files except .gitignore
-- Paste into linux/prodtest/bin
+- Paste into linux/prodtest/bin/
 
 hex-ftdi-cfg
 - Copy all P... and .fcf files from hex
-- Paste into window/hex-ftdi-cfg/hex
+- Paste into windows/hex-ftdi-cfg/hex/
 
 The End
---
+++ windows/hex-ftdi-cfg/hex/P115-cli.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem allow versions 3.17 and 3.18
+SET PATH=%PATH%;C:\Program Files (x86)\Renesas Electronics\Programming Tools\Renesas Flash Programmer V3.17
+SET PATH=%PATH%;C:\Program Files (x86)\Renesas Electronics\Programming Tools\Renesas Flash Programmer V3.18
+
+"RFPV3.Console.exe" "C:\EdgeCortix\hex-ftdi-cfg\hex\P115.rpj"
+
+echo Result Code should be zero (0): %ErrorLevel%
+
+rem removed for all.bat
+rem timeout 60
