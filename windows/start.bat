ff9131288059e3442a62175f1ecee6ed33531f1a
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-17 14:56:31 -0400

    added P115

--- windows/start.bat
+++ windows/start.bat
@@ -1,10 +1,25 @@
 @echo off
 
 python.exe barcode.py
+
+echo.
+echo Connect the Renesas Programmer.
+PAUSE 
+echo.
+
+call P115-cli.bat
+
+echo.
+echo Disconnect the Renesas Programmer 5x2.
+echo Optional: Cycle power, wait for the LED(s) to illuminate, and wait five more seconds.
+PAUSE
+echo.
+
 call ftdi.bat
 echo.
 color 07
-echo Confirm serial number then connect the USB to the linux machine.
+
+echo Confirm the serial number above, then connect the USB to the linux machine to continue.
 echo.
 echo Done.
 pause
