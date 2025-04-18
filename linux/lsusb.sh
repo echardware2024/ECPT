27fa25a5f0c8ea2a1544815d99645b2d6b5b0403
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-17 22:44:52 -0400

    use picocom

--- linux/lsusb.sh
+++ linux/lsusb.sh
@@ -1,6 +1,11 @@
-#!'bin/bash
+#!/bin/bash
 
+# get two lines of usb info and analyze
 lsusb -v -d 0403: 2> /dev/null | \grep -E "iProduct|iSerial" | awk '{$1=$1;print}' > lsusb.ftdi
 python3 ftdi.py
+
+# send over serial
+picocom -qrX -b 115200 --flow x --send-cmd ascii-xfr /dev/ttyUSB0
+cat ./cfg.edit | picocom -qrix 100 /dev/ttyUSB0
 
 # End
