44f6df7c648a560fb471ec455877048f74d37a06
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-17 21:53:22 -0400

    added prod and sn

--- linux/ftdi.py
+++ linux/ftdi.py
@@ -5,3 +5,13 @@ with open('lsusb.ftdi', 'r') as f:
 
 print('tinyusb')
 print (lines)
+
+if len(lines) == 2 and 'iProduct' in lines[0] and 'iSerial' in lines[1]:
+    product_stripped = lines[0].strip()
+    prod = product_stripped[-8:]
+    print(prod)
+    serial_stripped = lines[1].strip()
+    sn = serial_stripped [-8:]
+    print(sn)
+else:    
+    print('Invalid FTDI settings')
