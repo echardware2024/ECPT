af3db7b057184e296c992e4b6f84d194c2e6751f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-17 22:11:14 -0400

    check both now, for real

--- linux/ftdi.py
+++ linux/ftdi.py
@@ -16,19 +16,23 @@ def check_serial(s):
     return 'Invalid serial'
 # End
 
+# Start of script
 with open('lsusb.ftdi', 'r') as f:
     lines = f.readlines()
-print('tinyusb')
-print (lines)
+# print('tinyusb')  # debug
+# print (lines)  # debug
 
 if len(lines) == 2 and 'iProduct' in lines[0] and 'iSerial' in lines[1]:
     product_stripped = lines[0].strip()
     prod = product_stripped[-8:]
-    product = check_product(product_stripped[-8:])
+    product = check_product(prod)
     print(product)
 
     serial_stripped = lines[1].strip()
     sn = serial_stripped [-8:]
-    print(sn)
+    serial = check_serial(sn)
+    print(serial)
 else:    
     print('Invalid FTDI settings')
+
+# End
