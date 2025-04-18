4a1240631234ce92c4f8958496ce99999e061a7c
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-17 22:56:23 -0400

    1066

--- linux/lsusb.sh
+++ linux/lsusb.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# get two lines of usb info and analyze
+# get two lines of usb info then analyze
 lsusb -v -d 0403: 2> /dev/null | \grep -E "iProduct|iSerial" | awk '{$1=$1;print}' > lsusb.ftdi
-python3 ftdi.py
+python3 ftdi.py  # creates cfg.edit
 
 # send over serial
-picocom -qrX -b 115200 --flow x --send-cmd ascii-xfr /dev/ttyUSB0
-cat ./cfg.edit | picocom -qrix 100 /dev/ttyUSB0
+picocom -qrX -b 115200 --flow x --send-cmd ascii-xfr /dev/ttyUSB0  # setup
+cat ./cfg.edit | picocom -qrix 100 /dev/ttyUSB0  # send
 
 # End
 
+# debug: skip to end of current attempt
 # echo "/" | picocom -qrix 100 /dev/ttyUSB0
